ad82c6bbb0d4a25d596f11e83c55a0fb22a2dd7a
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2021-05-11 23:09:37 -0300

    feat(plugin): use new event API

--- data/plugins/statusicon_plugin.py
+++ data/plugins/statusicon_plugin.py
@@ -20,7 +20,7 @@ class StatusIconPlugin(plugin.Plugin):
         super().__init__()
         self.menu = None
         self.session = None
-        self.status_icon = self.create_widget()
+        self.widget = self.create_status_icon()
 
     def configure(self, bus: Bus, graph: Graph) -> None:
         super().configure(bus, graph)
@@ -46,35 +46,33 @@ class StatusIconPlugin(plugin.Plugin):
 
     @suppress_errors
     @on(Events.SESSION_START)
-    def show(self, *_, **__):
+    def show(self, **__):
         logger.debug("action=show")
-        self.status_icon.props.visible = True
+        self.widget.props.visible = True
 
     @suppress_errors
     @on(Events.SESSION_END, Events.SESSION_INTERRUPT)
-    def hide(self, *_, **__):
+    def hide(self, **__):
         logger.debug("action=hide")
-        self.status_icon.props.visible = False
-        self.status_icon.set_from_icon_name("tomate-idle")
+        self.widget.set_properties(visible=False, icon_name="tomate-idle")
 
     @suppress_errors
     @on(Events.TIMER_UPDATE)
-    def update_icon(self, _, payload: TimerPayload):
+    def update_icon(self, payload: TimerPayload):
         icon_name = self.icon_name(payload.elapsed_percent)
-        self.status_icon.set_from_icon_name(icon_name)
+        self.widget.props.icon_name = icon_name
         logger.debug("action=set_icon name=%s", icon_name)
 
     @staticmethod
     def icon_name(percent):
         return "tomate-{:02.0f}".format(percent)
 
-    def create_widget(self):
-        widget = Gtk.StatusIcon(visible=False)
-        widget.set_from_icon_name("tomate-idle")
-        widget.props.title = "TomateStatusIcon"
-        widget.connect("button-press-event", self.show_popup_menu)
-        widget.connect("popup-menu", self.show_popup_menu)
-        return widget
+    def create_status_icon(self):
+        status_icon = Gtk.StatusIcon.new_from_icon_name("tomate-idle")
+        status_icon.set_properties(visible=False, title="TomateStatusIcon")
+        status_icon.connect("button-press-event", self.show_popup_menu)
+        status_icon.connect("popup-menu", self.show_popup_menu)
+        return status_icon
 
     def show_popup_menu(self, *_):
         self.menu.widget.popup(None, None, None, None, 0, Gtk.get_current_event_time())
